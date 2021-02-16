02b5e9f1b43e1ba6d69e5fedcc7513d0049d8ec9
Jacob Passfield <jp1676@york.ac.uk>
2021-02-16 01:00:30 +0000

    Dum de dum.

--- temperature-size/temperature-size.R
+++ temperature-size/temperature-size.R
@@ -20,7 +20,61 @@ library(dplyr)
 
 # I tried, ask Gustav tomorrow
 
+attempt1 = semi_join(data, temporal_data)
+# no
+attempt2 = left_join(data, temporal_data)
+# no
+attempt3 = right_join(data, temporal_data)
+# no 
+
+## in their code
+load(file="temperature-size/_data/VertData_Temporal.RData")
+
+## remove rare observations
+df <- VertData_temporal 
+
+# decide which records to retain for analyses
+df_summary <- df %>%
+  group_by(TAXONOMIC_NAME, Loc, year) %>%
+  summarise(records = n()) %>%
+  filter(records >= 20) # must have 20 obs per year at a location
+
+# must be at least 20 obervations at a site within a given year
+df_ok <- semi_join(df, df_summary, 
+                   by = c("TAXONOMIC_NAME", "Loc", "year"))
+
+df_summary <- df_ok %>%
+  group_by(TAXONOMIC_NAME, Loc) %>%
+  summarise(
+    num_years = length(unique(year)) # years per location
+  ) %>%
+  filter(num_years >= 7) %>% # must have 8 years at a loc 
+  arrange(TAXONOMIC_NAME, Loc)
+
+# remove data 
+df_ok <- semi_join(df_ok, df_summary, 
+                   by = c("TAXONOMIC_NAME", "Loc")) 
+
+#select only relevant columns
+temporal_data <- df_ok %>% select(Location, SurveyID, SiteCode, Diver, TAXONOMIC_NAME, 
+                                  SPECIES_EPITHET, GENUS, FAMILY, ORDER, SizeClass, day, month, year, 
+                                  geogroup, SiteLat, SiteLong, midpoint, annSST, LWa, LWb, MaxLenFB, 
+                                  MaxSizeObs, mpa, Loc)
+
+##Add full location names for figures
+locnames <- list(NA)
+locnames$Loc <- unique(df_ok$Loc)
+locnames$LocFig <- c("Port Phillip Bay", "Maria Island", "Port Davey", "Jervis Bay", "Bicheno", "Tinderbox", "Bass Strait", "Jurien Bay", "Ninepin")
+
+temporal_data$LocFig <- locnames$LocFig[match(temporal_data$Loc, locnames$Loc)]
+
+# this is a copy of their code, with the end changed, plagarism?!
+# doesn't include 4 columns I want included, just in case
+
 # create data frame with the specified species
+# will use this new thing anyway
+
+
 
 
 
