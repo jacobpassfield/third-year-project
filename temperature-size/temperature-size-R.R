ebad86eb86bc6af0b87e697acc17e5ef148f0755
Jacob Passfield <jp1676@york.ac.uk>
2021-03-19 21:58:58 +0000

    Thursday's playing.

--- temperature-size/temperature-size-R.R
+++ temperature-size/temperature-size-R.R
@@ -1,22 +1,29 @@
 library(tidyverse)
 library(ggplot2)
 
-load(file = "_data/fish_data.RData")
+load(file = "temperature-size/_data/fish_data.RData")
 
 # Checking that there are 335 species
 length(unique(main_data$TAXONOMIC_NAME))
 # Creating a data frame to check how many occurances each species appear in the main data file
-speciesCount = as.data.frame(table(main_data$TAXONOMIC_NAME))
+speciesCount <- main_data %>% count(TAXONOMIC_NAME)
 
-# Creating data frame including yellow horsetail mackerel only
-YelHorMac <- main_data %>% filter(TAXONOMIC_NAME %in% "Trachurus novaezelandiae")
+# Creating data frame including yellow horse tail mackerel only
+YHM_data <- main_data %>% 
+                filter(TAXONOMIC_NAME %in% "Trachurus novaezelandiae")
+
+YHM_data$geogroup <- as.factor(YHM_data$geogroup)
+# Saving as a data file
+# save(YHM_data, file = "YHM_data.RData")
+# load(file = "temperature-size/_data/YHM_data.RData")
 
 # First basic linear model
-basic.lm <- lm(SizeClass ~ meansst, data = YelHorMac)
+basic.lm <- lm(SizeClass ~ meansst, data = YHM_data)
 summary(basic.lm)
 
-ggplot(YelHorMac, aes(x=meansst, y=SizeClass)) + geom_point() + geom_smooth(method="lm")
-
+ggplot(YHM_data, aes(x=meansst, y=SizeClass)) +
+  labs(y="Size class (cm)", x="Mean SST (°C)") +
+    geom_point() + geom_smooth(method="lm")
 
 # A plot of residuals against fitted values
 plot(basic.lm, which=1)
@@ -25,44 +32,77 @@ plot(basic.lm, which=1)
 plot(basic.lm, which=2)
 
 # Transforming data to hopefully lead to better modelling
-hist(YelHorMac$SizeClass)
+hist(YHM_data$SizeClass)
 
-YelHorMac <- mutate(YelHorMac, logSizeClass = log(SizeClass))
-hist(YelHorMac$logSizeClass)
+YHM_data <- mutate(YHM_data, logSizeClass = log(SizeClass))
+hist(YHM_data$logSizeClass)
 
-YelHorMac$scaledMSST <- scale(YelHorMac$meansst, center=T, scale=T)
+YHM_data$scaledMSST <- scale(YHM_data$meansst, center=T, scale=T)
 
-basic.lm2 <- lm(logSizeClass ~ scaledMSST, data = YelHorMac)
+basic.lm2 <- lm(logSizeClass ~ scaledMSST, data = YHM_data)
 summary(basic.lm2)
 
-(prelim_plot <- ggplot(YelHorMac, aes(x=scaledMSST, y=logSizeClass)) 
-   + geom_point() + geom_smooth(method="lm"))
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass)) +
+  labs(y="Size Class (cm)", x="Mean SST (°C)") +
+    geom_point() + geom_smooth(method="lm")
 
 plot(basic.lm2, which=1)
 
 plot(basic.lm2, which=2)
 
-length(unique(YelHorMac$geogroup))
+length(unique(YHM_data$geogroup))
 
-boxplot(SizeClass ~ geogroup, data = YelHorMac)
+#
+boxplot(SizeClass ~ geogroup, data = YHM_data, xlab="Geogroup", ylab="Size Class (cm)")
 
-(colour_plot <- ggplot(YelHorMac, aes(x=scaledMSST, y=logSizeClass, colour=geogroup)) +
-    geom_point(size=2) + theme_classic() + theme(legend.position="none"))
+ggplot(YHM_data, aes(x=as.factor(geogroup), y=SizeClass)) +
+  labs(y="Size class (cm)", x="Geogroup") +
+  geom_boxplot() + coord_flip()
 
-(split_plot <- ggplot(aes(scaledMSST, logSizeClass), data=YelHorMac) + geom_point() +
-    facet_wrap(~geogroup) + xlab("scaled mean sea surface temperature") + ylab("log transformed size class"))
+ggplot(YHM_data, aes(x=as.factor(Location), y=SizeClass)) +
+  labs(y="Size class (cm)", x="Location") +
+  geom_boxplot() + coord_flip()
 
-geo.lm <- lm(logSizeClass ~ scaledMSST + geogroup, data=YelHorMac)
+#
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass, colour=Location)) +
+  labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)") +
+    geom_point(alpha=0.1, size=3) + theme_classic() + theme(legend.position="none")
+
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass, colour="red")) + facet_wrap(~Location) +
+  theme(strip.text.x = element_text(size = 6.5)) +
+    labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)") +
+      geom_point(alpha=0.1, size=0.5) + theme(legend.position="none")
+
+geo.lm <- lm(logSizeClass ~ scaledMSST + Location, data = YHM_data)
 summary(geo.lm)
 
 library(lme4)
 
-mixed.lmer <- lmer(logSizeClass ~ scaledMSST + (1|geogroup), data=YelHorMac)
+mixed.lmer <- lmer(logSizeClass ~ scaledMSST +  (1|geogroup), data=YHM_data)
 summary(mixed.lmer)
+# Variance of geogroup
+0.18659/(0.18659+0.06451)
+# 0.7430904
 
 plot(mixed.lmer)
 
 qqnorm(resid(mixed.lmer))
 qqline(resid(mixed.lmer))
+
+# possibly wrong
+mixed.lmer2 <- lmer(logSizeClass ~ scaledMSST +  (1|geogroup) + (1|year), data=YHM_data)
+summary(mixed.lmer2)
+# Geogroup variance
+0.09219 / (0.09219+0.02929+0.05541) # 0.5211713
+# Year variance
+0.02929 / (0.09219+0.02929+0.05541) # 0.1655831
+
+# seeing if the different geogroups pop up in the same location
+PPH <- YelHorMac %>% filter(Location %in% "Sydney")
+length(unique(PPH$geogroup))
+SDY <- PPH %>% filter(geogroup > 1400)
+
+length(unique(YHM_data$geogroup))
+
 
           
