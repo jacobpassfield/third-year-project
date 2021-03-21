3a8de53783ce320414aed533698274d71375c1a7
Jacob Passfield <jp1676@york.ac.uk>
2021-03-21 21:45:33 +0000

    Committing files still being used.

--- temperature-size/temperature-size.R
+++ temperature-size/temperature-size.R
@@ -1,110 +1,214 @@
-data = load(file = "temperature-size/_data/fish_data.RData")
-data = main_data
+library(tidyverse)
+library(ggplot2)
 
-length(unique(data$TAXONOMIC_NAME))
-# 335 species
+load(file = "temperature-size/_data/fish_data.RData")
 
-# want to perform simple linear regression model on one species
-# then to perform multiple linear regression model
-# need to study mutliple linear regression models but need something to show tomorrow
+# Checking that there are 335 species
+length(unique(main_data$TAXONOMIC_NAME))
+# Creating a data frame to check how many occurances each species appear in the main data file
+speciesCount <- main_data %>% count(TAXONOMIC_NAME)
 
-speciesCount = as.data.frame(table(data$TAXONOMIC_NAME))
-# Trachurus novaezelandiae appear 914,584 times
+# Creating data frame including yellow horse tail mackerel only
+YHM_data <- main_data %>% 
+                filter(TAXONOMIC_NAME %in% "Trachurus novaezelandiae")
 
-# create a data frame with just the trachurus novaezelandiae species
-library(dplyr)
+YHM_data$geogroup <- as.factor(YHM_data$geogroup)
+YHM_data$SurveyID <- as.factor(YHM_data$SurveyID)
+YHM_data$year <- as.factor(YHM_data$year)
 
-# no create, new data frame inspired by the temporal_data file
-# Temporal data set with likely erroneous records and lower 10% filtered out and 
-# only for the 9 locations with long-term data available 
+# Saving as a data file
+# save(YHM_data, file = "YHM_data.RData")
+# load(file = "temperature-size/_data/YHM_data.RData")
+length(unique(YHM_data$SurveyID))
 
-# I tried, ask Gustav tomorrow
+# First basic linear model
+basic.lm <- lm(SizeClass ~ meansst, data = YHM_data)
+summary(basic.lm)
 
-attempt1 = semi_join(data, temporal_data)
-# no
-attempt2 = left_join(data, temporal_data)
-# no
-attempt3 = right_join(data, temporal_data)
-# no 
+ggplot(YHM_data, aes(x=meansst, y=SizeClass)) +
+  labs(y="Size class (cm)", x="Mean SST (°C)") +
+    geom_point() + geom_smooth(method="lm")
 
-## in their code
-load(file="temperature-size/_data/VertData_Temporal.RData")
+# A plot of residuals against fitted values
+plot(basic.lm, which=1)
 
-## remove rare observations
-df <- VertData_temporal 
+# Q-Q plot
+plot(basic.lm, which=2)
 
-# decide which records to retain for analyses
-df_summary <- df %>%
-  group_by(TAXONOMIC_NAME, Loc, year) %>%
-  summarise(records = n()) %>%
-  filter(records >= 20) # must have 20 obs per year at a location
+# Transforming data to hopefully lead to better modelling
+hist(YHM_data$SizeClass)
 
-# must be at least 20 obervations at a site within a given year
-df_ok <- semi_join(df, df_summary, 
-                   by = c("TAXONOMIC_NAME", "Loc", "year"))
+YHM_data <- mutate(YHM_data, logSizeClass = log(SizeClass))
+hist(YHM_data$logSizeClass)
 
-df_summary <- df_ok %>%
-  group_by(TAXONOMIC_NAME, Loc) %>%
-  summarise(
-    num_years = length(unique(year)) # years per location
-  ) %>%
-  filter(num_years >= 7) %>% # must have 8 years at a loc 
-  arrange(TAXONOMIC_NAME, Loc)
+YHM_data$scaledMSST <- scale(YHM_data$meansst, center=T, scale=T)
 
-# remove data 
-df_ok <- semi_join(df_ok, df_summary, 
-                   by = c("TAXONOMIC_NAME", "Loc")) 
+basic.lm2 <- lm(logSizeClass ~ scaledMSST, data = YHM_data)
+summary(basic.lm2)
 
-#select only relevant columns
-temporal_data <- df_ok %>% select(Location, SurveyID, SiteCode, Diver, TAXONOMIC_NAME, 
-                                  SPECIES_EPITHET, GENUS, FAMILY, ORDER, SizeClass, day, month, year, 
-                                  geogroup, SiteLat, SiteLong, midpoint, annSST, LWa, LWb, MaxLenFB, 
-                                  MaxSizeObs, mpa, Loc)
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass)) +
+  labs(y="Size Class (cm)", x="Mean SST (°C)") +
+    geom_point() + geom_smooth(method="lm")
 
-##Add full location names for figures
-locnames <- list(NA)
-locnames$Loc <- unique(df_ok$Loc)
-locnames$LocFig <- c("Port Phillip Bay", "Maria Island", "Port Davey", "Jervis Bay", "Bicheno", "Tinderbox", "Bass Strait", "Jurien Bay", "Ninepin")
+plot(basic.lm2, which=1)
 
-temporal_data$LocFig <- locnames$LocFig[match(temporal_data$Loc, locnames$Loc)]
+plot(basic.lm2, which=2)
 
-# this is a copy of their code, with the end changed, plagarism?!
-# doesn't include 4 columns I want included, just in case
-
-# create data frame with the specified species
-# will use this new thing anyway
-
-# "Size-classes of total fish length (from snout to tip of tail, or 
-# longest distance, including for stingrays) used are 2.5, 5.0, 7.5, 
-# 10.0, 12.5, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 50.0, 62.5 cm, and 
-# above. Lengths of fish larger than 62.5 cm should be estimated to 
-# the nearest 12.5 cm and individually recorded."
+length(unique(YHM_data$geogroup))
 
 
+# boxplot(SizeClass ~ geogroup, data = YHM_data, xlab="Geogroup", ylab="Size Class (cm)")
+
+ggplot(YHM_data, aes(x=as.factor(geogroup), y=SizeClass)) +
+  labs(y="Size class (cm)", x="Geogroup") +
+  geom_boxplot() + coord_flip()
+
+ggplot(YHM_data, aes(x=as.factor(Location), y=SizeClass)) +
+  labs(y="Size class (cm)", x="Location") +
+  geom_boxplot() + coord_flip()
+
+#
+# ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass, colour=Location)) +
+#  labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)") +
+#    geom_point(alpha=0.1, size=3) + theme_classic() + theme(legend.position="none")
+
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass, colour="red")) + facet_wrap(~Location) +
+  theme(strip.text.x = element_text(size = 6.5)) +
+    labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)") +
+      geom_point(alpha=0.1, size=0.5) + theme(legend.position="none")
+
+loc.lm <- lm(logSizeClass ~ scaledMSST + Location, data = YHM_data)
+summary(loc.lm)
+
+geo.lm <- lm(logSizeClass ~ scaledMSST + geogroup, data = YHM_data)
+summary(geo.lm)
+
+library(lme4)
+
+mixed.lmer <- lmer(logSizeClass ~ scaledMSST +  (1|Location), data=YHM_data)
+summary(mixed.lmer)
+# Variance of geogroup
+0.18659/(0.18659+0.06451) # 0.7430904
+# Variance of Location
+0.13940/(0.13940+0.06361) # 0.6866657
+
+plot(mixed.lmer)
+
+qqnorm(resid(mixed.lmer))
+qqline(resid(mixed.lmer))
+
+# CROSSED EFFECTS
+mixed.lmer2 <- lmer(logSizeClass ~ scaledMSST +  (1|Location) + (1|year), data=YHM_data)
+summary(mixed.lmer2)
+# Variance of location
+0.08785/(0.08785+0.02012+0.05485) # 0.5395529
+# Variance for year
+0.02012/(0.08785+0.02012+0.05485) # 0.123572
+plot(mixed.lmer2)
+
+# Geogroup variance
+0.09219 / (0.09219+0.02929+0.05541) # 0.5211713
+# Year variance
+0.02929 / (0.09219+0.02929+0.05541) # 0.1655831
+
+# NESTED EFFECTS
+mixed.lmer3 <- lmer(logSizeClass ~ scaledMSST + (1|Location/geogroup) + (1|year), data = YHM_data)
+summary(mixed.lmer3)
+# Geogroup within location variance 
+0.01927/(0.01927+0.07913+0.01925+0.05444) # 0.1119763
+# Location variance
+0.07913/(0.01927+0.07913+0.01925+0.05444) # 0.4598175
+# Year variance 
+0.01925/(0.01927+0.07913+0.01925+0.05444) # 0.1118601
+plot(mixed.lmer3)
+
+ggplot(YHM_data, aes(x=scaledMSST, y=logSizeClass, colour=geogroup)) +
+  facet_wrap(~Location) +   # a panel for each mountain range
+    geom_point(alpha = 0.1) +
+      theme_classic()  + theme(legend.position="none") + 
+        geom_line(data = cbind(YHM_data, pred = predict(mixed.lmer3)), aes(y = pred), size = 1)
+
+# Decided geogroup only, colour by year
+mixed.lmer4 <- lmer(logSizeClass ~ scaledMSST +  (1|geogroup) + (1|year), data=YHM_data)
+summary(mixed.lmer4)
+
+# Geogroup variance
+0.09219 / (0.09219+0.02929+0.05541) # 0.5211713
+# Year variance
+0.02929 / (0.09219+0.02929+0.05541) # 0.1655831
+
+# Following reference article, adding survey
+# ? Random variation among the surveys themselves (αi, for example, 
+# observer, site or weather differences). In this study a ‘survey’ is 
+# treated as a specific day in a given cell and may comprise several 50 m 
+# transect surveys.
+mixed.lmer5 <- lmer(logSizeClass ~ scaledMSST +  (1|geogroup) + (1|year) + (1|SurveyID), data=YHM_data)
+summary(mixed.lmer5) # does reduce correlation of fixed effects
+# SurveyID variance
+0.045014/(0.045014+0.071063+0.008575+0.019627) # 0.3119927
+# Geogroup
+0.071063/(0.045014+0.071063+0.008575+0.019627) # 0.4925388
+# Year variance
+0.008575/(0.045014+0.071063+0.008575+0.019627) # 0.05943346
+
+library(ggeffects)  # install the package first if you haven't already, then load it
+
+# Extract the prediction data frame
+pred.mm <- ggpredict(mixed.lmer5, terms = c("scaledMSST"))  # this gives overall predictions for the model
+# Plot the predictions 
+ggplot(pred.mm) + 
+    geom_line(aes(x = x, y = predicted)) +          # slope
+    geom_ribbon(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), 
+                fill = "lightgrey", alpha = 0.5) +  # error band
+    geom_point(data = YHM_data,                      # adding the raw data (scaled values)
+               aes(x = scaledMSST, y = logSizeClass, colour = geogroup)) + 
+  labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)", 
+         title = "Jacob") + 
+    theme_minimal() + theme(legend.position="none")
+
+##########################################################################
+
+# Creating data frame including black-spot goatfish only
+TSD_data <- main_data %>% 
+  filter(TAXONOMIC_NAME %in% "Dascyllus trimaculatus")
+
+TSD_data$geogroup <- as.factor(TSD_data$geogroup)
+TSD_data$SurveyID <- as.factor(TSD_data$SurveyID)
+TSD_data$year <- as.factor(TSD_data$year)
+
+TSD_data <- mutate(TSD_data, logSizeClass = log(SizeClass))
+
+TSD_data$scaledMSST <- scale(TSD_data$meansst, center=T, scale=T)
+
+mixed.lmer5 <- lmer(logSizeClass ~ scaledMSST +  (1|geogroup) + (1|year) + (1|SurveyID), data=TSD_data)
+summary(mixed.lmer5) # does reduce correlation of fixed effects
+
+pred.mm <- ggpredict(mixed.lmer5, terms = c("scaledMSST"))  # this gives overall predictions for the model
+
+ggplot(pred.mm) + 
+  geom_line(aes(x = x, y = predicted)) +          # slope
+  geom_ribbon(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), 
+              fill = "lightgrey", alpha = 0.5) +  # error band
+  geom_point(data = TSD_data,                      # adding the raw data (scaled values)
+             aes(x = scaledMSST, y = logSizeClass, colour = geogroup)) + 
+  labs(y="Log-transformed size class (cm)", x="Scaled mean SST (°C)", 
+       title = "Jacob") + 
+  theme_minimal() + theme(legend.position="none")
+
+ggpredict(mixed.lmer5, terms = c("scaledMSST", "geogroup [sample=9]"), type = "re") %>% 
+  plot() + 
+  labs(x = "SST", y = "Size", title = "Effect of temperature on body size in TSD") + 
+  theme_minimal()
+
+mixed.ranslope <- lmer(logSizeClass ~ scaledMSST + (1 + scaledMSST|geogroup), data = TSD_data) 
+
+summary(mixed.ranslope)
+
+library(sjPlot)
+plot_model(mixed.ranslope, type = "re", show.values = TRUE)
+
+# show summary
+summary(mixed.ranslope)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# https://ucdavis-bioinformatics-training.github.io/2018-September-Bioinformatics-Prerequisites/friday/linear_models.html
-
-# 1. Linear Models
-# mu = beta_0 + beta_1*x_1 + \epsilon
-# x: covariates, a continuous variable or a dummy variable
-# beta: unknown parameters to be estimated
-# epsilon: error term assumed to be normally distributed with constant variance
-
-# 2. Linear models in R
-str(data)
-# fit linear model using meansst as the 
